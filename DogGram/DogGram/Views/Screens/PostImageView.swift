5e0624fe29780e1e2bb5b9f2492c3b569bae3e96
James Phillips <jamesphillips2@icloud.com>
2021-09-27 17:51:39 -1000

    Add Settings Screen

---
+++ DogGram/DogGram/Views/Screens/PostImageView.swift
@@ -0,0 +1,82 @@
+//
+//  PostImageView.swift
+//  DogGram
+//
+//  Created by James Phillips on 9/27/21.
+//
+
+import SwiftUI
+
+struct PostImageView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    @State var captionText: String = ""
+    @Binding var imageSelected: UIImage
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 0) {
+            
+            HStack {
+                Button {
+                    presentationMode.wrappedValue.dismiss()
+                } label: {
+                    Image(systemName: "xmark")
+                        .font(.title)
+                        .padding()
+                }
+                .accentColor(.primary)
+                
+                Spacer()
+            }
+            ScrollView(.vertical, showsIndicators: false) {
+                
+                Image(uiImage: imageSelected)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 200, height: 200, alignment: .center)
+                    .cornerRadius(12)
+                    .clipped()
+                
+                TextField("Add your caption here", text: $captionText)
+                    .padding()
+                    .frame( height: 60)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.MyTheme.beigeColor)
+                    .cornerRadius(12)
+                    .font(.headline)
+                    .padding(.horizontal)
+                    .autocapitalization(.sentences)
+                
+                Button {
+                    postPicture()
+                } label: {
+                    Text("Post Picture!".uppercased())
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .padding()
+                        .frame( height: 60)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.MyTheme.purpleColor)
+                        .cornerRadius(12)
+                        .padding(.horizontal)
+                }
+                .accentColor(Color.MyTheme.yellowColor)
+            }
+            
+        }
+    }
+    //MARK: - Functions
+    func postPicture(){
+        print("post picture to database here")
+    }
+}
+
+struct PostImageView_Previews: PreviewProvider {
+    
+    @State static var image = UIImage(named: "dog1")!
+    
+    static var previews: some View {
+        PostImageView( imageSelected: $image)
+        
+    }
+}
---
+++ DogGram/DogGram/Views/Subviews/SettingsLabelView.swift
@@ -0,0 +1,36 @@
+//
+//  SettingsLabelView.swift
+//  DogGram
+//
+//  Created by James Phillips on 9/27/21.
+//
+
+import SwiftUI
+
+struct SettingsLabelView: View {
+    
+    var labelText: String
+    var labelImage: String
+    
+    var body: some View {
+        VStack {
+            HStack{
+                Text(labelText)
+                    .fontWeight(.bold)
+                   
+                Spacer()
+                
+                Image(systemName: labelImage)
+            }
+            Divider()
+                .padding(.vertical, 4)
+        }
+    }
+}
+
+struct SettingsLabelView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsLabelView(labelText: "Test Label", labelImage: "heart")
+            .previewLayout(.sizeThatFits)
+    }
+}
